a684411a4a90f93bc2d5388f561b7c50747245bb
Amir <60965542+amir4rab@users.noreply.github.com>
2023-04-15 05:59:01 +0000

    added preact-ts-scss-template config files

---
+++ preact-ts-scss-template/rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    preserveModules: true,
+  },
+  plugins: [
+    typescript(),
+    postcss({
+      extract: true,
+      autoModules: true,
+    }),
+  ],
+  external: ['preact', 'preact/jsx-runtime'],
+};
